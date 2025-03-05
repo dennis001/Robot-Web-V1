950d358c4a38dc28a39571fbb9c25aa77c925752
unknown <dennisdesousa.ds@gmail.com>
2025-03-05 14:19:22 -0300

    Refacture estrutura da adesão de matricula

--- resources/libs/Database.py
+++ resources/libs/Database.py
@@ -1,18 +1,27 @@
 import psycopg2
 
-def delete_account_by_email(email):
-    
-    query = f"DELETE FROM accounts WHERE email = '{email}';"
-
-    conn = psycopg2.connect(
-        host='localhost',
-        database='smartbit',
-        user='postgres',
-        password='QAx@123',
+db_conn = """
+    host='localhost'
+        dbname='smartbit'
+        user='postgres'
+        password='QAx@123'
         port='5432'
-    )
+"""
 
+def execute(query):
+    conn = psycopg2.connect(db_conn)
     cur = conn.cursor()
     cur.execute(query)
     conn.commit()
     cur.close()
+
+def insert_account(account):
+    query = f"""INSERT INTO accounts (name,email,cpf) 
+    VALUES ('{account["name"]}', '{account["email"]}', '{account["cpf"]}');
+    """
+    execute(query)
+
+
+def delete_account_by_email(email):
+    query = f"DELETE FROM accounts WHERE email = '{email}';"
+    execute(query)
